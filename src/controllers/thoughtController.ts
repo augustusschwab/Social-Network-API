8307ce149a40ed3836d18650109a46a8b770c381
Gus <gus@Augustuss-MacBook-Pro-2.local>
2025-03-27 13:09:20 -0600

    added reaction routes

--- src/controllers/thoughtController.ts
+++ src/controllers/thoughtController.ts
@@ -86,3 +86,41 @@ export const deleteThought = async (req: Request, res: Response) => {
         return res.status(500).json(err);
     }
 }
+
+// Add a reaction to a thought.
+export const addReaction = async (req: Request, res: Response) => {
+    try{
+        const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $push: {reactions: req.body} },
+            { runValidators: true, new: true}
+        )
+
+        if(!thought){
+            res.status(404).json({ message: 'Thought does not exist.' });
+        }
+
+        res.json(thought)
+    } catch(err) {
+        res.status(500).json(err);
+    }
+};
+
+// Remove a reaction from a thought.
+export const removeReaction = async (req: Request, res: Response) => {
+    try{
+        const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: {reactions: { reactionId: req.params.reactionId }}},
+            { new: true }
+        );
+
+        if(!thought){
+            res.status(404).json({ message: 'Thought not found.' });
+        }
+
+        res.json(thought);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+};
